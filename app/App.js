f1d5567fd50f9efcfea056c9ae35f41c78cadbc7
Volodymyr Huzar <vguzar@i.ua>
2018-02-24 23:03:32 +0100

    Move active piece left & right using arrow keys

--- app/App.js
+++ app/App.js
@@ -2,7 +2,8 @@ import {Application, Container, loader, Sprite} from 'pixi.js/dist/pixi.min'
 import assets from '../assets/*'
 import {createBackground} from './Background'
 import {generateActivePiece} from './ActivePiece'
-import {moveDown} from './spriteUtils'
+import {moveDown} from './pieceMovementHandlers'
+import {getKeyDownHandler} from './pieceKeyHandlers'
 
 export const createApp = ({tileSize, wideCells, heightCells}) => {
 	const appConfig = {width: tileSize * wideCells, height: tileSize * heightCells}
@@ -10,6 +11,7 @@ export const createApp = ({tileSize, wideCells, heightCells}) => {
 	loader.add(Object.entries(assets)).load(() => {
 		app.stage.addChild(createBackground(appConfig, tileSize))
 		const activePiece = generateActivePiece()
+		window.addEventListener('keydown', getKeyDownHandler(activePiece, appConfig, tileSize))
 		app.stage.addChild(activePiece)
 		app.ticker.add(moveDown(activePiece, appConfig.height))
 	})
--- app/spriteUtils.js
+++
@@ -1,3 +0,0 @@
-export const moveDown = (sprite, floor) => () => {
-	if (sprite.y < floor - sprite.height) sprite.y += 1
-}
